6acde629d3da7fb1a5ce70583713a919cc581348
thaaer234 <mhmadwerc8@gmail.com>
2026-05-11 18:07:10 +0300

    تحديثات المنصة

--- config/settings.py
+++ config/settings.py
@@ -227,6 +227,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
