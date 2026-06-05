f72e6ebfe6431e2a5122076a3e17b5f5c93b7ad9
thaaer234 <mhmadwerc8@gmail.com>
2026-06-05 17:53:22 +0300

    تحديثات المنصة

--- scratch_test.py
+++ scratch_test.py
@@ -5,8 +5,7 @@ django.setup()
 
 from django.template import loader
 try:
-    loader.get_template("dashboard/device_logged_out.html")
+    loader.get_template("dashboard/admin_student_detail.html")
     print("TEMPLATE COMPILES OK!")
 except Exception as e:
     print("TEMPLATE ERROR:", str(e))
-
